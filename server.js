14c9173eeccaaf709c19021a0e8a3371be8f707e
Regan Dufort <regan.t.dufort@gmail.com>
2018-12-12 11:36:22 -0800

    database setup complete?

--- server.js
+++ server.js
@@ -9,28 +9,44 @@ const superagent = require('superagent');
 const pg = require('pg');
 const ejs = require('ejs');
 
-
+// pull in project specific enviroment variables
 require('dotenv').config();
 
+//specific database connection path
+//TODO: 
+const client = new pg.Client(`${DATABASE_URL}`);
+client.connect();
+client.on('error', err => console.error(err)); 
 
-// setup app canstants
+// setup app constants
 const PORT = process.env.PORT;
 const app = express();
 
-// set view engine
+// set server side view engine
 app.set('view engine', 'ejs');
 
-// allow public acces to our api
+// allow public acces to our api - middleware
 app.use(cors());
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 
 
 // routes
-
 app.get('/', (request, response) => {
   response.render('../public/views/pages/index');
 });
+// path from db
+app.get('/', booksDB);
+
+function booksDB(request, response) {
+  // our table NEEDS to be named books with this syntax
+  let SQL = 'SELECT * from books;';
+
+  return client.query(SQL)
+  // check pathway on 'index' pathway - tablename.rows as of now table is books
+  .then( results => response.render('index', { results: books.rows }))
+  .catch(err => console.error(err));
+}
 
 
 app.post('/searches', getBooks);
@@ -66,9 +82,8 @@ function getBooks(request, response) {
     .catch(error => console.log(error));
 }
 
-
-
-
+// catch-all route
+app.get('*', (req, res) => res.status(404).send('Not Found'));
 
 // listening to server
 app.listen(PORT, () => {
